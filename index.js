1e7ae2df3ed7acd4dc05e2926d152ff5e6f96ed7
Xavier Defontaine <xdefontaine@gmail.com>
2024-10-28 15:36:25 +0000

    chore: re-add hasVersionChanged output for CIs to skip steps

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ const run = async () => {
     const newVersion = bumpVersion(changelog, oldVersion);
 
     core.info(`version: ${oldVersion} => ${newVersion}`);
-
-    if (oldVersion === newVersion) {
+    const hasVersionChanged = oldVersion === newVersion
+    if (hasVersionChanged) {
       if (shouldThrowIfNoVersionBump === 'true') throw ('No version change')
       else core.notice('No version change');
       return true;
@@ -65,6 +65,7 @@ const run = async () => {
 
     core.setOutput('changelog', changelog);
     core.setOutput('version', newVersion);
+    core.setOutput('hasVersionChanged', hasVersionChanged);
   } catch (error) {
     core.setFailed(error.message);
   }
